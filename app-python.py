700ebc8aac9a1219a0b29ae26a969a9bda86ebed
shereenm1521 <shereinmakram@gmail.com>
2023-10-31 15:19:49 +0300

    fdbf

--- app-python.py
+++ app-python.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'Hello this is shereen from GKE')
+    target = os.environ.get('TARGET', 'this is shereen from GKE')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
